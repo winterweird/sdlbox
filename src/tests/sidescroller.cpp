2bb06b3a0cb17aed9bdeafe84cebee3a62597b67
Vegard Itland <Vegard.Itland@student.uib.no>
2017-06-26 02:49:51 +0200

    Not sure, something about the sidescroller example
    It's not really looking much like a side scroller, though.
    
    I apologize for the crappy commit messages, but I'm a little bit tired right
    now.

--- src/tests/sidescroller.cpp
+++ src/tests/sidescroller.cpp
@@ -5,11 +5,68 @@
 using namespace std;
 using namespace sdlbox;
 
-class Runner : public Component {
+class GameObject : public Component {
     public:
-        Runner(int x, int y) {
+        GameObject(int x, int y, const Color &color, double vSpeed, double hSpeed) : color(color), xPos(x), yPos(y), vSpeed(vSpeed), hSpeed(hSpeed) {
             ComponentFactory(this).position(x, y);
         }
+
+        void draw() const override {
+            auto r = this->getRect();
+            GraphicsHelper::drawRect(&r, color);
+        }
+
+        void vAccel(double goal) {
+            vSpeed = goal;
+        }
+
+        void vAccel(double goal, double amount) {
+            if (goal > vSpeed)
+                vSpeed = min(vSpeed + amount, goal);
+            else
+                vSpeed = max(vSpeed - amount, goal);
+        }
+
+        void hAccel(double goal) {
+            hSpeed = goal;
+        }
+
+        void hAccel(double goal, double amount) {
+            if (goal > hSpeed)
+                hSpeed = min(hSpeed + amount, goal);
+            else
+                hSpeed = max(hSpeed - amount, goal);
+        }
+
+        virtual void cap() {}
+        
+        void cap(double minX, double maxX, double minY, double maxY) {
+            xPos = max(minX, min(xPos, maxX));
+            yPos = max(minY, min(yPos, maxY));
+        }
+
+        void step() override {
+            // subclass behavior goes here
+            
+            // this might become troublesome
+            xPos += hSpeed;
+            yPos += vSpeed;
+
+            cap();
+            
+            ComponentFactory(this).position(round(xPos), round(yPos));
+        }
+        
+    private:
+        Color color;
+
+        double xPos, yPos; // keep track of actual position
+        double vSpeed, hSpeed; // init from subclass constr
+};
+
+class Runner : public GameObject {
+    public:
+        Runner(int x, int y) : GameObject(x, y, Color(0, 255, 0), 0, 0) { }
 
         int getWidth() const override {
             return 20;
@@ -17,18 +74,23 @@ class Runner : public Component {
         int getHeight() const override {
             return 40;
         }
-        void draw() const override {
-            auto r = this->getRect();
-            GraphicsHelper::drawRect(&r, Color(0, 255, 0));
+
+        void cap() override {
+            GameObject::cap(0, 800-getWidth(), 0, 250);
+            
+            if (getY() == 250) {
+                grounded = true;
+                vAccel(0);
+            }
         }
 
         void step() override {
-            double goalX = getX(), goalY = getY();
+            hAccel(0); // reset hSpeed
             
             const Uint8* keys = SDL_GetKeyboardState(NULL);
             if (keys[SDL_SCANCODE_W]) {
                 if (grounded) {
-                    vspeed = -vspeedBoost;
+                    vAccel(-vspeedBoost);
                     grounded = false;
                 }
             }
@@ -38,38 +100,18 @@ class Runner : public Component {
                 }
             }
             if (keys[SDL_SCANCODE_A]) {
-                goalX -= hspeed;
+                hAccel(-2, 2);
             }
             if (keys[SDL_SCANCODE_D]) {
-                goalX += hspeed;
+                hAccel(2, 2);
             }
             
             if (!grounded) {
-                // update y position
-                // On a side note, magic numbers are bad, and 250 is where the
-                // "floor" is (or, 250 + height, really)
-
-                goalY += vErr + vspeed;
-                
-                // it doesn't look like this line makes much of a difference, but meh
-                vErr = goalY - (int)round(goalY); // could also use some cmath function or w/e
-                
-                if (goalY >= 250.0) {
-                    grounded = true;
-                    vspeed = 0;
-                    vErr = 0;
-                }
-                else {
-                    vspeed += gravity;
-                }
-
+                // "100" is just some random big value
+                vAccel(100, gravity);
             }
 
-            // normalize goals
-            goalX = min(max(0.0, goalX), 800.0-getWidth());
-            goalY = min(250.0, goalY);
-
-            ComponentFactory(this).position(round(goalX), round(goalY));
+            GameObject::step();
         }
     private:
         // NOTE: naïve approach to positioning and speed. What I used in Flappy
@@ -89,10 +131,67 @@ class Runner : public Component {
         // variable declarations.
         static const int hspeed = 2;
         constexpr static const double gravity = 0.2;
-        double vErr = 0;
+        
         bool grounded = true;
-        double vspeed = 0;
         constexpr static const double vspeedBoost = 6.0;
+};
+
+class Obstacle : public GameObject {
+    public:
+        Obstacle(int x, int y, double hSpeed, int width, int height) : GameObject(x, y, Color(255, 0, 0), 0, -hSpeed), width(width), height(height) {}
+
+        int getWidth() const override {
+            return width;
+        }
+
+        int getHeight() const override {
+            return height;
+        }
+
+        void step() override {
+            GameObject::step();
+            if (getX() < -getWidth()) {
+                SDLBox::getInstance()->scheduleDestruct(this);
+            }
+        }
+    private:
+        int width, height;
+};
+
+class ObstacleSpawner : public Component {
+    public:
+        ObstacleSpawner(int x, int y) {
+            ComponentFactory(this).position(x, y);
+        }
+
+        void step() override {
+            if (--timer <= 0) {
+                auto win = SDLBox::getInstance();
+                int width = win->randint(20, 50);
+                int height = win->randint(20, 40);
+                int ypos = win->getHeight() - win->randint(minAboveBottom, maxAboveBottom) - height;
+                int speed = win->randint(10, 30);
+                win->add(new Obstacle(getX(), ypos, speed/10.0, width, height));
+                timer = cooldown;
+            }
+        }
+
+        int getWidth() const override {
+            return -1; // N/A
+        }
+        int getHeight() const override {
+            return -1; // N/A
+        }
+        void draw() const override { }
+
+        bool drawable() const override {
+            return false;
+        }
+    private:
+        int timer = 0;
+        const int cooldown = 100;
+        const int maxAboveBottom = 100;
+        const int minAboveBottom = 10;
 };
 
 int main(int argc, char** argv) {
@@ -103,8 +202,12 @@ int main(int argc, char** argv) {
     Runner* runner = new Runner(200, 250);
 
     window.add(runner);
+    window.add(new ObstacleSpawner(840, 0));
 
     mainloop(&window);
+    
+    // I sorta lost touch with what I was doing...
+    // TODO: figure that out.
 
     return 0;
 }
